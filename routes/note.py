8026d349c931ee43a943b60f6bd9185a12f33678
shivam <shivamnegi479@gmail.com>
2023-06-09 12:34:47 +0530

    refind the post method to inserting todos in database

--- routes/note.py
+++ routes/note.py
@@ -6,12 +6,10 @@ from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 note =APIRouter()
-from pydantic import BaseModel
+from typing import Dict
+
 templates = Jinja2Templates(directory="templates")
 
-# class User(BaseModel):
-#     name:str
-#     email:str
 @note.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     docs=conn.fastapi.fastapi.find({})
@@ -21,12 +19,30 @@ async def read_item(request: Request):
         newdoc.append({"id":i["_id"],"title":i["title"],"desc":i["desc"],"important":i["important"]})
     return templates.TemplateResponse("index.html", {"request": request,"notes":newdoc})
 
-@note.post("/create_notes/")
-async def create_user(notes: Note):
-    note_data = notes.dict()
-    print(notes)
-    print(note_data["title"])
-    if conn.fastapi.fastapi.find_one({"title": note_data['title']}):
+@note.post("/")
+async def create_user(request:Request):
+
+    form=await request.form()
+
+    data=dict(form)
+    if "important" not in data.keys():
+        data["important"]=False
+    else:
+        data["important"]=True 
+   
+    if conn.fastapi.fastapi.find_one({"title": data['title']}):
         raise HTTPException(status_code=400, detail="Note with the same title already exists.")
-    notes_id = conn.fastapi.fastapi.insert_one(note_data).inserted_id
-    return {"Message":"Notes Created Successfully","note":notes}
+    notes_id = conn.fastapi.fastapi.insert_one(data)
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@note.post("/dsa")
+def hhe():
+    return {
+        "tye":"testt"
+    }
+
+@note.get("/user")
+def user():
+    return {
+        "user":"Shivam"
+    }
